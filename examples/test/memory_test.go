3d5d28409c1b8215a1b92ba2052569d60c1f55c3
alex.guoba@gmail.com <alex.guoba@gmail.com>
2016-10-30 19:21:21 +0800

    add test demo

---
+++ examples/test/memory_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+func heap() []byte {
+	return make([]byte, 1024*10)
+}
+
+func BenchmarkHeap(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = heap()
+	}
+}
