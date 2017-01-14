fd2d26f2fb1eee8e1a9aaf53ad00a3dc0c68d380
alex.guoba@gmail.com <alex.guoba@gmail.com>
2017-01-14 18:54:26 +0800

    loop variable is reused for each iteration

--- examples/for/for.go
+++ examples/for/for.go
@@ -4,26 +4,35 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 
-    // The most basic type, with a single condition.
-    i := 1
-    for i <= 3 {
-        fmt.Println(i)
-        i = i + 1
-    }
+	// The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
 
-    // A classic initial/condition/after `for` loop.
-    for j := 7; j <= 9; j++ {
-        fmt.Println(j)
-    }
+	// A classic initial/condition/after `for` loop.
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
 
-    // `for` without a condition will loop repeatedly
-    // until you `break` out of the loop or `return` from
-    // the enclosing function.
-    for {
-        fmt.Println("loop")
-        break
-    }
+	// `for` without a condition will loop repeatedly
+	// until you `break` out of the loop or `return` from
+	// the enclosing function.
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	// in a Go for loop, the loop variable is reused for each iteration
+	for m := 0; m < 4; m++ {
+		go func() {
+			fmt.Println(m)
+		}()
+	}
+	time.Sleep(time.Second)
 }
